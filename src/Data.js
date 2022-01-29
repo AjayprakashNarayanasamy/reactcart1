8de0c94da81281659019596528aaefc385130fa2
Ajay Prakash <ajayprakash@AGLS-MLT-433.local>
2022-01-29 19:46:50 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,56 @@
+import pic from './image.jpeg';
+import pic1 from './coke.jpeg';
+const data={
+    product:[
+        {
+            id:1,
+            name:"COKE",
+            image:{pic},
+            price:80.00
+        },
+        {
+            id:2,
+            name:"PEPSI",
+            image:{pic},
+            price:18.00
+        },
+        {
+            id:3,
+            name:"APPLE",
+            image:{pic},
+            price:25.00
+        },
+        {
+            id:4,
+            name:"MANGO",
+            image:{pic},
+            price:40.00
+        },
+        {
+            id:5,
+            name:"GRAPE",
+            image:{pic},
+            price:25.00
+
+        },
+        {
+            id:6,
+            name:"LEMON JUICE",
+            image:{pic},
+            price:120
+        },
+        {
+            id:7,
+            name:"MILK SHAKE",
+            image:{pic},
+            price:18
+        },
+        {
+            id:8,
+            name:"JUICE",
+            image:{pic},
+            price:40
+        }
+    ]
+}
+export default data
